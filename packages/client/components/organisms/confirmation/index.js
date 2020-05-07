90ab77d9389d84fb75cfb7a6d16c018d3cae872a
Gabriel Taveira <gabrielstaveira@gmail.com>
2020-05-07 19:00:23 -0300

    Changing happy icon

--- packages/client/components/organisms/confirmation/index.js
+++ packages/client/components/organisms/confirmation/index.js
@@ -1,20 +1,28 @@
 import React from "react";
-import { Fade } from '@material-ui/core';
+import { Fade } from "@material-ui/core";
 import { Focus, Button, Title, Description } from "components/atoms";
-import { Box, Shadow, Container, Worker } from './styles';
+import { Box, Shadow, Container, Laywer } from "./styles";
 
 export default ({ onClose, open, ...rest }) => (
   <Fade in={open}>
-    <Focus  {...rest}>
-      {open && <Box>
-        <Worker src="/static/happy_worker.svg" height={150} width={150} />
-        <Shadow />
-        <Container>
-          <Title.h2><span>Cadastrado!</span></Title.h2>
-          <Description>Obrigado por se inscrever! Avisaremos assim que tudo ficar pronto.</Description>
-          <Button filled radius="100px" onClick={onClose}>Ok</Button>
-        </Container>
-      </Box>}
+    <Focus {...rest}>
+      {open && (
+        <Box>
+          <Laywer src="/static/happy_laywer.svg" height={150} width={150} />
+          <Shadow />
+          <Container>
+            <Title.h2>
+              <span>Registered!</span>
+            </Title.h2>
+            <Description>
+              Thank you! We will get in contact as soon as possible.
+            </Description>
+            <Button filled radius="100px" onClick={onClose}>
+              Ok
+            </Button>
+          </Container>
+        </Box>
+      )}
     </Focus>
   </Fade>
 );
--- packages/client/components/organisms/confirmation/styles.js
+++ packages/client/components/organisms/confirmation/styles.js
@@ -1,61 +1,71 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 import { Button, Description, Title } from "components/atoms";
 
 export const Box = styled.div`
-    max-width: 400px;
-    margin: 40px;
-    width: 100%;
-    
-    border: 1px solid #DDDDDD;
-    background-color: white;
-    border-radius: 15px;
-    color: black;
-    @keyframes bounce {
-        20%{transform: scaleY(1) }
-        50%{transform: translateY(-50px) scaleY(1.1) }
-        90%{transform: scaleY(.9)}
+  max-width: 400px;
+  margin: 40px;
+  width: 100%;
+
+  border: 1px solid #dddddd;
+  background-color: white;
+  border-radius: 15px;
+  color: black;
+  @keyframes bounce {
+    20% {
+      transform: scaleY(1);
     }
-    @keyframes scale {
-        20%{transform: scaleX(1) }
-        50%{transform: scaleX(.9) }
-        90%{transform: scaleX(1.1)}
+    50% {
+      transform: translateY(-50px) scaleY(1.1);
     }
+    90% {
+      transform: scaleY(0.9);
+    }
+  }
+  @keyframes scale {
+    20% {
+      transform: scaleX(1);
+    }
+    50% {
+      transform: scaleX(0.9);
+    }
+    90% {
+      transform: scaleX(1.1);
+    }
+  }
 `;
-export const Worker = styled.img`
-    position: relative;
-    top: 15px;
-    animation: bounce 1s ease-in infinite;
-    z-index: 1;
-`
+export const Laywer = styled.img`
+  position: relative;
+  top: 15px;
+  animation: bounce 1s ease-in infinite;
+  z-index: 1;
+`;
 export const Shadow = styled.div`
-    position: relative;
-    top: -2px;
-    width: 100px;
-    height: 15px;
-    opacity: .5;
-    background: rgba(0,0,0,.2);
-    border-radius: 50%;
-    animation: scale 1s ease-in infinite;
-    margin: auto;
-`
+  position: relative;
+  top: -2px;
+  width: 100px;
+  height: 15px;
+  opacity: 0.5;
+  background: rgba(0, 0, 0, 0.2);
+  border-radius: 50%;
+  animation: scale 1s ease-in infinite;
+  margin: auto;
+`;
 export const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 20px;
-    ${Title.h2}{
-        font-size: 30px;
-        margin: 0 0 15px 0;
-    }
-    ${Description}{
-        margin-bottom: 25px;
-    }
-    ${Button}{
-        height: 20px;
-        font-size: 18px;
-        width: 80%;
-        margin: 0 0 15px 0;
-    }
-`
-
-
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  ${Title.h2} {
+    font-size: 30px;
+    margin: 0 0 15px 0;
+  }
+  ${Description} {
+    margin-bottom: 25px;
+  }
+  ${Button} {
+    height: 20px;
+    font-size: 18px;
+    width: 80%;
+    margin: 0 0 15px 0;
+  }
+`;
